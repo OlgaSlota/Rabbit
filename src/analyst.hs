2088c26a45145e23e1a8da397cac9c9481e65839
Wojciech Baczyński <baczynskiwojtek@gmail.com>
2017-06-07 17:02:25 +0200

    Little visual changes.

--- src/analyst.hs
+++ src/analyst.hs
@@ -8,32 +8,60 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.ByteString.Lazy.Char8 as BL
-import          System.Environment
+import System.Environment
+import System.Exit
 import Data.Map as M
 import Network.AMQP
 import Network.AMQP.Types
+import Data.Char
+import Data.List    -- for intercalate
 
 main = do
-    (bodyPart:_) <- getArgs
+    specs <- getArgs
 
+    if length specs /= 1 then do
+        putStrLn "Give 1 specialization !"
+        putStr   "("
+        putStr   (intercalate " / " specializations)
+        putStrLn ")"
+        exitFailure
+    else
+        putStr ""
+
+    (bodyPart:_) <- getArgs
+    arg0 : _ <- getArgs
+
+    putStrLn "+ --------- +"
+    putStr   "|  ANALYST  | ("
+    putStr   (fmap toUpper arg0)
+    putStrLn ")\n+ --------- +"
+
+    -- Connection
     conn <- openConnection "127.0.0.1" "/" "guest" "guest"
     chan <- openChannel conn
 
+    -- Get declared Queue (created in tech)
     (replyToQ, _, _) <- declareQueue chan anonQueue
 
+    -- Declare Exchange
     declareExchange chan newExchange {exchangeName = "hospital-exchange", exchangeType = "direct"}
 
+    putStrLn "Running... Press Enter to close.\n"
+
+
+    -- Publish to the Queue
     publishMsg chan "hospital-exchange" "" (msg replyToQ bodyPart)
     consumeMsgs chan replyToQ Ack acceptReply
 
-    getLine -- wait for keypress
 
+    getLine -- wait for keypress
     closeConnection conn
     putStrLn "connection closed"
 
   where
-    msg rk bodyPart = (newMsg { msgBody = (BL.pack $ bodyPart)
-                  , msgReplyTo = Just rk })
+    specializations = ["analysis", "algebra", "logic"]
+    msg rk bodyPart = newMsg { msgBody = (BL.pack $ bodyPart),
+                                msgReplyTo = Just rk }
 
 anonQueue :: QueueOpts
 anonQueue = QueueOpts "" False False True True (FieldTable M.empty)
@@ -41,5 +69,5 @@ anonQueue = QueueOpts "" False False True True (FieldTable M.empty)
 
 acceptReply :: (Message, Envelope) -> IO ()
 acceptReply (msg, env) = do
-    putStrLn $ "Request result: " ++ (BL.unpack $ msgBody msg)
+    putStrLn $ ">> " ++ (BL.unpack $ msgBody msg)
     ackEnv env
---
+++ src/math.hs
@@ -0,0 +1,87 @@
+#!/usr/bin/env stack
+{- stack --install-ghc
+    runghc
+    --package amqp
+    --package bytestring
+    --package text
+-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString.Lazy.Char8 as BL
+import System.Environment
+import System.Exit
+import Data.Maybe (fromJust)
+import Data.Map as M
+import Network.AMQP
+import Data.Char
+import Data.List    -- for intercalate
+
+main = do
+    specs <- getArgs
+
+    if length specs /= 1 then do
+        putStrLn "Give 1 specialization !"
+        putStr   "("
+        putStr   (intercalate " / " specializations)
+        putStrLn ")"
+        exitFailure
+    else
+        putStr ""
+
+    arg0 : _ <- getArgs
+
+    putStrLn "+ --------------- +"
+    putStr   "|  MATHEMATICIAN  | ("
+    putStr   (fmap toUpper arg0)
+    putStrLn ")\n+ --------------- +"
+
+    -- Connection
+    conn      <- openConnection "127.0.0.1" "/" "guest" "guest"
+    chan      <- openChannel conn
+    replyChan <- openChannel conn
+
+    -- Declare Queue and binding
+    declareQueue chan newQueue {queueName = "hospital-queue"}
+    bindQueue chan "hospital-queue" "hospital-exchange" ""
+
+    -- Declare Exchange
+    declareExchange chan newExchange {exchangeName = "hospital-exchange", exchangeType = "direct"}
+
+    putStrLn "Running... Press Enter to close.\n"
+
+
+    -- Subscribe to the Queue
+    consumeMsgs chan "hospital-queue" Ack (processMessage replyChan)
+
+
+    getLine -- wait for keypress
+    closeConnection conn
+    putStrLn "connection closed"
+
+  where
+      specializations = ["analysis", "algebra", "logic"]
+
+--processMessage :: Channel -> (Message, Envelope) -> IO ()
+processMessage chan (msg, env) = do
+  arg0 : _ <- getArgs
+  if bodyPart == arg0 then do
+      publishMsg chan "" routingKey reply
+      ackEnv env
+  else do
+      putStr   "I cannot help with `"
+      putStr   bodyPart
+      putStrLn  "` problem :("
+
+  where
+    -- extract routing key
+    routingKey = fromJust $ msgReplyTo msg
+    -- build reply
+    reply = newMsg {msgBody = (BL.pack (getReply bodyPart))}
+    -- extract body part
+    bodyPart = BL.unpack $ msgBody msg
+    solutions = M.fromList[("analysis", "Analysis done"), ("algebra", "Algebra done"), ("logic", "Logic done")]
+    -- lookup in the Map
+    getReply bodyPart =
+      case M.lookup bodyPart solutions of
+        Just v  -> v
+        Nothing -> ""
